f5e93828d84cd5ff7d00cce73b3e4d1f42b1bc68
Haryormidey <akinsanyaayomide99@gmail.com>
2022-06-22 15:50:00 +0100

    first commit

---
+++ first.js
@@ -0,0 +1,5 @@
+
+alert('Name: Akinsanya Precious Ayomide \nHeight: 5ft 7inches \nCountry: Nigeria')
+
+// let details = 'Name: Akinsanya Precious Ayomide \nHeight: 5ft 7inches \nCountry: Nigeria';
+// console.log(details);
